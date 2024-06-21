90af8a586e490e804708b930f2cca3f78f5d573d
3tosin <adeniyioluwafaith@gmail.com>
2024-06-21 15:57:56 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/hello', pathMatch: 'full' }, // Default route
+  {
+    path: 'hello',
+    component: HelloComponent,
+  },
+];
